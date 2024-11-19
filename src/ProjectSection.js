bf44e9b3966e095d971fb71482c04a9c1795f88f
Rohith Kumar B <rohithkumarcbaalraj@gmail.com>
2024-11-19 09:53:45 +0530

    Made changes in project

--- src/ProjectSection.js
+++ src/ProjectSection.js
@@ -8,7 +8,7 @@ function ProjectSection() {
   const obj = [
     {title: "Souled", text: "The jersey and boots purchasing website allows users to browse and buy sports apparel. It features user authentication for secure accounts, an intuitive interface with HTML and CSS, and PHP for handling transactions and managing inventory."},
     {title: "Gamercult", text: "The game purchase website includes user authentication and OTP verification. Users register, log in, and manage profiles with HTML, CSS, and PHP."},
-    {title: "Contact List", text: "Create a contact list application that allows users to add, delete, and update contacts. The UI will include forms for adding and editing contacts, and options to remove contacts. The app will feature a responsive design and dynamic interactions using JavaScript."},
+    {title: "Pharmacy Management", text: "Developed a website for managing pharmacy. Leveraged HTML5, CSS3,JS and PHP to create a responsive design and an intuitive user interface ."},
     {title: "Invoice Generator", text: "The basic invoice generator uses HTML for structure, CSS for styling, and JavaScript for dynamic functionality. Users can input item details, quantities, and prices to automatically generate and display an invoice with calculated totals and formatting."}
   ];
 
